69d89121c5702b1c156878c2262b5a95b678ac80
nlll <insanless@gmail.com>
2017-04-03 10:13:12 +0300

    Update locators.py

--- Util/locators.py
+++ Util/locators.py
@@ -15,17 +15,17 @@ locators_login_page = {'loc_login_user': '#user_email',
                        'loc_login_title': '.app-logo-login'}
 
 locators_oauth_page = {'oauth_title':'/html/body/div[2]/div[1]/ul[2]/li[8]/a',
-'add_title':'//*[@id="pane2"]/div[1]/div',
-'field_name':'//*[@id="oauth_applications_name"]',
-'form_app':'//*[@id="new_oauth_application"]/div[3]',
-'add_button':'//*[@id="pane2"]/div[2]/a',
-'url_field':'//*[@id="oauth_applications_redirect_uri"]',
-'save_btn':'//*[@id="new_oauth_application"]/div[3]/div[2]/input',
-'alert_msg':'/html/body/div[2]/div[1]/div/div',
-'delete_btn':'//*[@id="drop1"]/li[2]/a',
-'settings_el':'//*[@id="pane3"]/div/div[1]/div[1]/a/div',
-'edit_btn':'//*[@id="drop1"]/li[1]/a',
-'cancel_btn':'//*[@id="new_oauth_application"]/div[3]/div[2]/a/span'}
+                      'add_title':'//*[@id="pane2"]/div[1]/div',
+                      'field_name':'//*[@id="oauth_applications_name"]',
+                      'form_app':'//*[@id="new_oauth_application"]/div[3]',
+                      'add_button':'//*[@id="pane2"]/div[2]/a',
+                      'url_field':'//*[@id="oauth_applications_redirect_uri"]',
+                      'save_btn':'//*[@id="new_oauth_application"]/div[3]/div[2]/input',
+                      'alert_msg':'/html/body/div[2]/div[1]/div/div',
+                      'delete_btn':'//*[@id="drop1"]/li[2]/a',
+                      'settings_el':'//*[@id="pane3"]/div/div[1]/div[1]/a/div',
+                      'edit_btn':'//*[@id="drop1"]/li[1]/a',
+                      'cancel_btn':'//*[@id="new_oauth_application"]/div[3]/div[2]/a/span'}
 
 locators_team_page = {}
 
